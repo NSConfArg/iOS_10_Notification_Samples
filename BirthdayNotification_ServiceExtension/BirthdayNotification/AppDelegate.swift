0f4e07b7ef5a8f487eeed24f340206aad35ff3a2
Silvina Roldan <silvinaroldan@gmail.com>
2017-04-02 19:15:41 -0300

    Payload change and ask authorization requested

--- BirthdayNotification_ServiceExtension/BirthdayNotification/AppDelegate.swift
+++ BirthdayNotification_ServiceExtension/BirthdayNotification/AppDelegate.swift
@@ -17,7 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .badge, .sound], completionHandler:
+      {(granted, error) in 
+        application.registerForRemoteNotifications()
+    })
+    
     return true
   }
 
